ee43e178e43c85a15bf5657c0b2ff219a00212e7
Rumin <tan.rumin@gmail.com>
2023-07-24 03:27:28 +0000

    Connect to database and include flask app config setting

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="rumin",
+    password="sql12345",
+    hostname="rumin.mysql.pythonanywhere-services.com",
+    databasename="rumin$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 comments = []
 
 @app.route("/", methods=["GET", "POST"])
