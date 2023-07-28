4c61c36e876c2542fbd462cebd821973a22fe621
Rumin <tan.rumin@gmail.com>
2023-07-28 14:56:04 +0000

    Added a timestamp

--- flask_app.py
+++ flask_app.py
@@ -58,13 +58,14 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now)
 
 #comments = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all(), timestamp=datetime.now())
+        return render_template("main_page.html", comments=Comment.query.all())
 
     if not current_user.is_authenticated:                                       #check whether user is logged in, before saving his comment in database
         return redirect(url_for('index'))
